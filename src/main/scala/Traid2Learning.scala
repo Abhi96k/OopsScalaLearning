df551e5a41e262321d150b18bbeef8b698872b2a
Abhi96k <abhisheknangare96k@gmail.com>
2025-05-25 21:09:12 +0530

    add Animal, Dog, and Crocodile classes to demonstrate abstract classes and traits in Scala

--- src/main/scala/Traid2Learning.scala
+++ src/main/scala/Traid2Learning.scala
@@ -1,5 +1,33 @@
 package OopsLearning
 
 object Traid2Learning extends App {
-  
+
+  abstract class Animal {
+    val creatureType: String
+    def eat(): Unit
+  }
+  class Dog extends Animal {
+    override val creatureType: String = "Canine"
+    override def eat(): Unit = println("Dog eats bones")
+  }
+
+  trait carnivore {
+    def eat(animal: Animal): Unit
+  }
+
+  class Crocodile extends Animal with carnivore {
+    override val creatureType: String = "Crocodile"
+    override def eat(): Unit = println("Crocodile eats meat")
+
+    override def eat(animal: Animal): Unit = {
+      println(s"Crocodile eats ${animal.creatureType}")
+    }
+  }
+
+  val dog = new Dog
+  val crocodile = new Crocodile
+  crocodile.eat(dog) // Output: Crocodile eats Canine
+  dog.eat() // Output: Dog eats bones
+  crocodile.eat() // Output: Crocodile eats meat
+
 }
