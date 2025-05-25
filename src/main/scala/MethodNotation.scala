edbb708a725d5d5298ba78d489726a4ee08377c0
Abhi96k <abhisheknangare96k@gmail.com>
2025-05-25 09:55:00 +0530

    add hangoutName and unary_! methods to MethodNotation; demonstrate usage with Person instances

--- src/main/scala/MethodNotation.scala
+++ src/main/scala/MethodNotation.scala
@@ -9,9 +9,32 @@ object MethodNotation extends App {
     def likes(movie:String):Boolean={
       movie == favroiteMovie
     }
+
+    def hangoutName(person: Person):String=s"${this.name} hangs out with ${person.name}"
+
+    def + (person: Person): String = {
+      s"${this.name} is friends with ${person.name}"
+    }
+
+    //prefix operator Notation
+    def unary_! : String = s"$name, what the heck are you doing?"
+
+    //postfix operator Notation
+    //availbe only in without parameters
+    def isAlive: String = s"$name is alive"
+
+
   }
    val person = new Person("Abhishek", "Inception")
    println(person.likes("Inception")) // true
    println(person likes "Inception") // true, using infix notation
+
+   val person2=new Person("John", "Avatar")
+    println(person hangoutName person2) // Abhishek hangs out with John
+
+    println(person + person2) // Abhishek is friends with John
+    println(person.+ (person2)) // Abhishek is friends with John, using method notation
+
+
 
 }
